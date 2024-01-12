c227425645702fe3a532fb98cfe263040f2e844f
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-12 13:08:09 -0800

    adding modulus

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if ((number % 10) > 5):
-    print(f"Last digit of {number} is and is greater than 5")
-if ((number % 10) == 0):
-    print(f"Last digit of {number} is and is 0")
-if ((abs(number % 10)) < 6):
-    print(f"Last digit of {number} is and is less than 6 and not 0")
+last = (number % 10)
+if (last > 5):
+    print(f"Last digit of {number} is {last} and is greater than 5")
+if ((last) == 0):
+    print(f"Last digit of {number} is {last} and is 0")
+if ((abs(last)) < 6):
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
