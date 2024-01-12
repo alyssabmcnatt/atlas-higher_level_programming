a98b17b38807d375265affc906ca8fe0da8d1d30
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-12 13:18:33 -0800

    getting that neg to show

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 last = (number % 10)
+last_neg = (number % -10)
 if (last > 5):
     print(f"Last digit of {number} is {last} and is greater than 5")
 if (last == 0):
     print(f"Last digit of {number} is {last} and is 0")
-if (abs(last) < 6):
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+if (last < 6):
+    print(f"Last digit of {number} is {last_neg} and is less than 6 and not 0")
