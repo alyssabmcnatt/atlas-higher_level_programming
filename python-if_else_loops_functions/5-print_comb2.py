dcd6091d1e6a03e1eb624b7e83fdd8a17cfa7455
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-13 13:01:49 -0800

    taking away unnecessary commas

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 number = 0
 while number <= 98:
-    print("{0:02d}".format(number,), end=", ")
+    print("{0:02d}".format(number), end=", ")
     number += 1
     if number == 99:
-        print("{0:02d}".format(number,), end="")
+        print("{0:02d}".format(number), end="")
