83487192af28f7c859aceb75f5cc04223f09b897
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-16 00:00:51 -0800

    2 blank lines

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -17,7 +17,7 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -25,11 +25,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-    
+
     def my_print(self):
         """Print the square with the # character."""
         for i in range(0, self.__size):
