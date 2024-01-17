2e1caf6b600c8aca102df038996894216a470c94
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-17 13:20:49 -0800

    pycodestyle errors

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,14 +16,16 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrix representing the result of the division.
     """
-    if (not isinstance(matrix, list) or matrix == [] or not all(isinstance(row, list)
-                                                                for row  in matrix) or not all((isinstance(ele, int) or isinstance(ele, float))
-                                                                                               for ele in [num for row in matrix for num in row])):
+    if (not isinstance(matrix, list) or matrix == [] or not
+        all(isinstance(row, list) for row  in matrix) or not
+        all((isinstance(ele, int) or isinstance(ele, float))
+            for ele in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists)"
                         + "of integers/floats")
     
     if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError("Each row of the matrix must have"
+                        + "the same size")
     
     if not isinstance(div, int) and isinstance(div, float):
         raise TypeError("div must be a number")
@@ -31,4 +33,5 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("dividion by zero")
     
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    return ([list(map(lambda x: round(x / div, 2), row))
+             for row in matrix])
