f5c999f44962de5ea2ab1b420119e53c872e664c
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-17 15:28:29 -0800

    pycode errors

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -63,5 +63,5 @@ class Rectangle:
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
- 
+
         return ("".join(rect))
