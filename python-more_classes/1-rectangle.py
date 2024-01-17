7ec3d4ffd9de8e2158d8a98f154e159d3d40c207
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-17 15:24:46 -0800

    pycode errors

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-        
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -20,7 +20,7 @@ class Rectangle:
     def width(self):
         """Get/set the width of the rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -33,7 +33,7 @@ class Rectangle:
     def height(self):
         """Get/set the height of the rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-        
+ 
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -20,7 +20,7 @@ class Rectangle:
     def width(self):
         """Get/set the width of the Rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -33,7 +33,7 @@ class Rectangle:
     def height(self):
         """Get/set the height of the Rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -45,7 +45,7 @@ class Rectangle:
     def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
-    
+
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
