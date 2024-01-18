58b4326ab177c18fdf2fc9c3f0c41eafaccf4795
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-17 17:10:16 -0800

    rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-        
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -20,7 +20,7 @@ class Rectangle:
     def width(self):
         """Get/set the width of the Rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -33,7 +33,7 @@ class Rectangle:
     def height(self):
         """Get/set the height of the Rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -45,27 +45,27 @@ class Rectangle:
     def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
-    
+
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-    
+
     def __str__(self):
         """Return the printable representation of the Rectangle.
         Represent the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
-        
+
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-    
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
