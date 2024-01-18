4f8455879cceb92191cdf7b98d9b4f0d3491b7f1
alyssabmcnatt <6654@holbertonstudents.com>
2024-01-17 17:17:41 -0800

    rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
-        Represent the rectangle with the # character.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
